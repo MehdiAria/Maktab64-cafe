fddf3d79721affc1da53c5ec6aa26ff20553817b
MehdiAria <mahdishahidi010@gmail.com>
2022-01-01 14:24:14 +0330

    Add: create method

--- core/db-manager.py
+++ core/db-manager.py
@@ -20,5 +20,22 @@ class DBManager:
         self.conn: connection = psycopg2.connect(dbname=self.database, user=self.user, host=self.host, port=self.port,
                                                  password=self.password)
 
+    def create(self, model_instance: DBModel) -> int:
+        with self.conn:
+            assert isinstance(model_instance, DBModel)
+            curs = self.__get_cursor()
+            model_vars = vars(model_instance)
+            model_fields_str = ",".join(
+                model_vars.keys())
+            model_values_str = ",".join(["%s"] * len(model_vars))
+            model_values_tuple = tuple(model_vars.values())
+            with curs:
+                curs.execute(
+                    f"""INSERT INTO {model_instance.TABLE}({model_fields_str}) VALUES ({model_values_str}) RETURNING ID;""",
+                    model_values_tuple)
+                id = int(curs.fetchone()['id'])
+                setattr(model_instance, 'id', id)
+                return id
+
 
 db1 = DBManager()
