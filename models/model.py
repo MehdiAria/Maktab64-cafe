227b72a5fb723d749714502c0b4009df5db797df
bloodyfork <mohtor.138055@gmail.com>
2022-01-01 17:19:28 +0330

    fixed TABLE names and id

--- models/model.py
+++ models/model.py
@@ -1,76 +1,85 @@
-from core.db_manager import DBModel
-from datetime import datetime
+from core.db_manager import DBModel, DBManager
+from datetime import datetime, timedelta
 
 
 class Cashier(DBModel):
-    def __init__(self, _id, name, last_name, email, phone, password) -> None:
-        self._id = id
+    TABLE = "cashier"
+
+    def __init__(self, name, last_name, email, phone, password, id=0) -> None:
         self.name = name
         self.last_name = last_name
         self.email = email
         self.phone = phone
         self.password = password
+        self.id = id
 
 
 class CafeTable(DBModel):
-    def __init__(self, _id, number, space) -> None:
-        self._id = _id
+    TABLE = "cafe_table"
+
+    def __init__(self, number, space, id) -> None:
         self.number = number
         self.space = space
+        self.id = id
 
 
 class MenuItems(DBModel):
+    TABLE = 'menu_items'
 
-    def __init__(self, _id, category_id, discount, name, price, img_url, serving_time) -> None:
-        self._id = id
+    def __init__(self, category_id, discount, name, price, image_url, serving_time, id=0) -> None:
         self.category_id = category_id
         self.name = name
         self.discount = discount
         self.price = price
-        self.img_url = img_url
+        self.image_url = image_url
         self.serving_time = serving_time
-
-
-class Table(DBModel):
-    def __init__(self, _id, number, space):
-        self.id = _id
-        self.number = number
-        self.space = space
+        self.id = id
 
 
 class Status(DBModel):
-    def __init__(self, _id, name, description):
-        self.id = _id
+    TABLE = "status"
+
+    def __init__(self, name, description, id=0):
         self.name = name
         self.description = description
+        self.id = id
 
 
 class Category(DBModel):
-    _id: int
-    name: str
-    category_id: int
+    TABLE = "categories"
 
-    def __init__(self, _id, name, category_id=None):
-        self._id = _id
+    def __init__(self, name, category_id=None, id=0):
         self.name = name
         self.category_id = category_id
+        self.id = id
 
 
 class Order(DBModel):
-    def __init__(self, _id, item_id, number_items, receipt_id, status_id, table_id):
-        self._id = id
+    TABLE = "orders"
+
+    def __init__(self, item_id, number_item, receipt_id, status_id, table_id, id=0):
         self.item_id = item_id
-        self.number_items = number_items
+        self.number_item = number_item
         self.receipt_id = receipt_id
         self.status_id = status_id
         self.table_id = table_id
         self.time_stamp = datetime.now()
+        self.id = id
 
 
 class Receipt(DBModel):
-    def __init__(self, _id, total_price, final_price) -> None:
-        self._id = _id
+    TABLE = "receipt"
+
+    def __init__(self, total_price, final_price, id=0) -> None:
+
         self.total_price = total_price
         self.final_price = final_price
         self.time_stamp = datetime.now()
+        self.id = id
 
+
+# cat = Category("cake")
+# db1 = DBManager().create(cat)
+time_t = datetime.now() + timedelta(minutes=10)
+item = MenuItems(1, 0, 'cake', 50000, 'img_url', time_t)
+db = DBManager().create(item)
