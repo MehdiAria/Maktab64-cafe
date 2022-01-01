f3730c2e6fc5547fde78fe270b440765fd049e89
bloodyfork <mohtor.138055@gmail.com>
2022-01-01 15:58:30 +0330

    write an Order model

--- models/model.py
+++ models/model.py
@@ -1,5 +1,5 @@
 from core.db_manager import DBModel
-
+from datetime import datetime
 
 class Cashier(DBModel):
     def __init__(self, id, name, last_name, email, phone, password) -> None:
@@ -9,3 +9,16 @@ class Cashier(DBModel):
         self.email = email
         self.phone = phone
         self.password = password
+
+
+
+
+class Order(DBModel):
+    def __init__(self,id, item_id, number_items, receipt_id, status_id, table_id):
+        self.id = id
+        self.item_id = item_id
+        self.number_items = number_items
+        self.receipt_id = receipt_id
+        self.status_id = status_id
+        self.table_id = table_id
+        self.time_stamp = datetime.now()
