54a2b9db66d0823c6e47ddddac4841f489c4f677
bloodyfork <mohtor.138055@gmail.com>
2022-01-02 00:28:45 +0330

    started front-end and faced connecting css bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
@@ -7,8 +7,8 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('base_layout/_base.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
