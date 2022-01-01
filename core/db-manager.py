f4d094e0ffa9b3b7b33e4876aa56e5ecaa2ab489
m.kh <khosroshahi89@gmail.com>
2022-01-01 14:24:37 +0330

    Add DBModel Abstract class to db-manager.py

--- core/db-manager.py
+++ core/db-manager.py
@@ -1,5 +1,16 @@
 import psycopg2
 from psycopg2._psycopg import connection, cursor
+from abc import ABC
+
+
+class DBModel(ABC):  # abstract base Database model
+    TABLE: str  # table name
+    PK: str  # primary key column of the table
+
+    def __str__(self) -> str:
+        return f"<{self.__class__.__name__} {vars(self)}>"
+
+
 
 
 class DBManager:
