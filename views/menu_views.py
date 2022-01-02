5360a1ec07a6abb78b84f54ae39ca0453b38a338
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-03 00:49:21 +0330

    reformat: menu_views.py

--- views/menu_views.py
+++ views/menu_views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from core.db_manager import DBManager
 from models.model import *
+
 db = DBManager()
+
 
 def index():
     return render_template("index.html")
@@ -9,7 +11,5 @@ def index():
 
 def menu():
     items = db.read_all(MenuItems)
-    print(items)
-    data = {"menu_items": items}
-    print(data['menu_items'])
+    data = {"items": items}
     return render_template("menu.html", data=data)
