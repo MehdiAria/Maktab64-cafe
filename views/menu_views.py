3495c12581a7640052da6f8051d03f5da4876c70
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-02 19:06:19 +0330

    reformat: menu_views.py

--- views/menu_views.py
+++ views/menu_views.py
@@ -2,5 +2,6 @@ from flask import render_template
 
 
 def index():
-    data = ["static/"]
-    return render_template("index.html")
+    data = ["static/images/milk_shake.jpg", "static/images/tea.jpg", "static/images/mocha.jpg",
+            "static/images/hot_chocolate.jpg"]
+    return render_template("index.html", data=data)
