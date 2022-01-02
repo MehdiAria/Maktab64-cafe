4b4f59cb98ab2d5260f01fcdad73a290a24f8790
MehdiAria <mahdishahidi010@gmail.com>
2022-01-03 00:36:27 +0330

    add: about page template

--- views/about.py
+++ views/about.py
@@ -2,4 +2,13 @@ from flask import render_template
 
 
 def about():
-    pass
+    title = "About Us"
+    text = """
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda delectus doloremque fugit, maiores perferendis quas similique ullam velit vero? Atque excepturi exercitationem illum quia.
+    """
+    img=""
+    date = {
+        'title': title,
+        'text': text,
+    }
+    return render_template('about.html', data=date)
