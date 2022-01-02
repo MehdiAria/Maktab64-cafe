ea756b1f738088406bb182221b290b58d43c4439
MehdiAria <mahdishahidi010@gmail.com>
2022-01-02 23:57:02 +0330

    add: about page init

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from views.menu_views import *
+from views.about import *
 
 app = Flask(__name__, template_folder="templates")
 
@@ -8,5 +9,7 @@ app = Flask(__name__, template_folder="templates")
 
 app.add_url_rule("/", "home", index)
 app.add_url_rule("/menu", "menu", menu)
+app.add_url_rule("/about", "about", about)
+
 if __name__ == '__main__':
     app.run(debug=True)
