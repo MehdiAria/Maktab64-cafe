a33f0f7e718320e6e40f061cb55b46415cc5cc1b
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-02 22:53:14 +0330

    reformat: app.py

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ app = Flask(__name__, template_folder="templates")
 
 # JINJA_ENVIRONMENT.globals['STATIC_PREFIX'] = '/'
 
-@app.route('/')
-def base():  # put application's code here
-    return render_template('base.html')
 
 
-app.add_url_rule("/index", "home", index)
+app.add_url_rule("/", "home", index)
 app.add_url_rule("/menu", "menu", menu)
 if __name__ == '__main__':
     app.run(debug=True)
