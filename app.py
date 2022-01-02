b58c2a047ec7c660157643737c3aab85a0c56a69
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-02 18:41:26 +0330

    Add: /index (route)

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, url_for
-
-
-
+from views.menu_views import *
 
 app = Flask(__name__, template_folder="templates")
+
 
 # JINJA_ENVIRONMENT.globals['STATIC_PREFIX'] = '/'
 
@@ -11,6 +10,8 @@ app = Flask(__name__, template_folder="templates")
 def base():  # put application's code here
     return render_template('base.html')
 
+
+app.add_url_rule("/index", "home", index)
 
 if __name__ == '__main__':
     app.run(debug=True)
