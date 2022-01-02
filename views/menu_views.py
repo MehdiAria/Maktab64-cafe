c48beb3b5bc47da6bbdef13c916342a571d0e8fe
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-02 20:06:34 +0330

    Add: menu.html

--- views/menu_views.py
+++ views/menu_views.py
@@ -2,6 +2,10 @@ from flask import render_template
 
 
 def index():
+    return render_template("index.html")
+
+
+def menu():
     data = ["static/images/milk_shake.jpg", "static/images/tea.jpg", "static/images/mocha.jpg",
             "static/images/hot_chocolate.jpg"]
-    return render_template("index.html", data=data)
+    return render_template("menu.html", data=data)
