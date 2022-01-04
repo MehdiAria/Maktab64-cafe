8f32d6109923f35bff045d9ba593af87696b6922
bloodyfork <mohtor.138055@gmail.com>
2022-01-04 15:57:17 +0330

    reformating order view function Done!

--- views/menu_views.py
+++ views/menu_views.py
@@ -30,10 +30,17 @@ def order(table_id):
         # return Response('Your order created!', 201)
         receipt_id = request.cookies.get('receipt_id', None)
         order_dict = request.form
+        db = DBManager()
         if receipt_id:
             order1 = Order(item_id=order_dict.get('item_id'), table_id=order_dict.get('table_id'),
                        status_id=0, number_item=order_dict.get('number_item'), receipt_id=receipt_id)
+            db.create(order1)
         else:
-            receipt = Receipt(0, 0)
+            price = db.read(MenuItems, int(order_dict.get("item_id"))).price * int(order_dict.get("number_item"))
+            receipt = Receipt(total_price=price, final_price=0)
+            db.create(receipt)
+            order1 = Order(item_id=order_dict.get('item_id'), table_id=order_dict.get('table_id'),
+                           status_id=0, number_item=order_dict.get('number_item'), receipt_id=receipt.id)
+            db.create(order1)
         return 'good', 201
 
