861957145f5661a9571231e628a0cf2355bfd7be
m.kh <khosroshahi89@gmail.com>
2022-01-05 21:31:56 +0330

    add: register function and register_view.html

--- views/login.py
+++ views/login.py
@@ -5,3 +5,44 @@ from models.model import *
 
 def login():
     return render_template('login.html')
+
+
+
+def register():
+    title = "Register form"
+    text = """ """
+    img = ""
+    date = {
+        'title': title,
+        'text': text,
+    }
+
+    data['user'] = get_user_by_cookie(request, users)
+
+
+    if request.method == "GET":
+        # form view !
+        return render_template("register_view.html", data=data)
+
+    elif request.method == "POST":
+        # get user field
+        name = escape(request.form.get('name'))
+        password = escape(request.form.get('password'))
+
+        for user in users:
+            if user['name'] == name:
+                return "User exists!", 500
+
+        # register successful !
+        user = {
+            "id": len(users),
+            "name": escape(request.form.get('name')),
+            "password": escape(request.form.get('password')),
+            "key": '',
+        }
+
+        users.append(user)
+
+        return redirect(url_for('home'))
+
+    return "Forbidden Request 403", 403
