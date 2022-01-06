668d3e8abcc36eb4f56ce74fcef6dc51ec6f2795
m.kh <khosroshahi89@gmail.com>
2022-01-06 19:47:16 +0330

    edit: get_user_by_cookie function

--- views/utils.py
+++ views/utils.py
@@ -1,11 +1,15 @@
-def get_user_by_cookie(request, cashiers: list):
+from core.db_manager import DBManager
+from models.model import Cashier
+
+def get_user_by_cookie(request, cashiers):
     cashier_id = request.cookies.get('cashier_logged_in_id', None)
     cashier_token = request.cookies.get('cashier_logged_in_key', None)
 
-    print(cashier_id, cashier_token)
+    # print(cashier_id, cashier_token)
 
-    for cashier in cashiers:
-        if cashier.token == cashier_token and cashier.id == cashier_id:
-            return cashier
-
-    return None
+    if cashier_id == None or cashier_token == None:
+        return None
+    else:
+        db = DBManager()
+        cashier = db.read_filter(Cashier, token=cashier_token)
+        return cashier
