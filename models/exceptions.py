161ac054a0e880c1fad99080f00be79a48552a3f
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-08 14:26:32 +0330

    reformat: exceptions.py

--- models/exceptions.py
+++ models/exceptions.py
@@ -2,4 +2,11 @@ class CreateOrderError(Exception):
     """
     This Error would raise when the Order.__init__ get any invalid value
     """
-    pass
+    def __init__(self, field, data, msg=""):
+        super().__init__(data, msg)
+        self.data = data
+        self.field = field
+        self.msg = msg
+
+    def __str__(self):
+        return f"{self.field} can not get {self.data}!{self.msg}"
