4f6e79b1efa7f31a1eccd575cd040caef429b7cf
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-10 00:06:06 +0330

    reformat: receipts_view.py , receipts.html

--- cashier/receipts_view.py
+++ cashier/receipts_view.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import render_template
 from core.db_manager import DBManager
 from models.model import Receipt
 from datetime import datetime, timedelta
@@ -9,4 +9,4 @@ db = DBManager()
 def time_receipts(time_filter=0):
     receipt_date_time = datetime.now() - timedelta(days=time_filter)
     date_receipts = db.read_filter(Receipt, f"time_stamp >= {receipt_date_time}")
-    return f"{date_receipts}"
+    return render_template("cashier/receipts.html", data=date_receipts)
