0eb2da580a8f49a19ca886c3d93e8c6a30440301
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-09 23:44:53 +0330

    Add: time_receipts route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from views.login import *
 from cashier import tables
 from cashier import orders
 from cashier import edit_items
-
+from cashier.receipts_view import *
 app = Flask(__name__, template_folder="templates")
 
 # JINJA_ENVIRONMENT.globals['STATIC_PREFIX'] = '/'
@@ -19,7 +19,7 @@ app.add_url_rule("/panel", "panel", panel)
 app.add_url_rule("/order/<string:table_id>", "order", order, methods=['GET', 'POST'])
 app.add_url_rule("cashier/edit_items", "edit_items", edit_items)
 app.add_url_rule("cashier/orders", "orders", orders)
-app.add_url_rule("cashier/receipts", "receipts", receipts)
+app.add_url_rule("cashier/date_receipts/<int:time_filter>", "date_receipts", time_receipts)
 # app.add_url_rule("cashier/served_orders", "served_orders", served_orders)
 app.add_url_rule("cashier/tables", "tables", tables)
 
