97d41bc867ec64ae34563905ce654bd05553da46
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-10 01:04:53 +0330

    fix: bug in time_receipts query

--- cashier/receipts_view.py
+++ cashier/receipts_view.py
@@ -7,6 +7,7 @@ db = DBManager()
 
 
 def time_receipts(time_filter=0):
-    receipt_date_time = datetime.now() - timedelta(days=time_filter)
-    date_receipts = db.read_filter(Receipt, f"time_stamp >= {receipt_date_time}")
-    return render_template("cashier/receipts.html", data=date_receipts)
+    receipt_date_time = datetime.now() - timedelta(hours=time_filter)
+    data = db.all_query(Receipt, f"""SELECT * FROM receipt WHERE time_stamp >= CAST ('{receipt_date_time}' AS timestamp)""")
+    return render_template("cashier/receipts.html", data=data)
+
