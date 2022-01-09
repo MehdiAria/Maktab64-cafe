02cb3c3ee52121d6deb7b03f6f50809d3ac6f93f
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-09 23:37:15 +0330

    Add: time_receipts

---
+++ cashier/receipts_view.py
@@ -0,0 +1,11 @@
+from flask import *
+from core.db_manager import DBManager
+from models.model import Receipt
+from datetime import datetime, timedelta
+db = DBManager()
+def time_receipts(time_filter=0):
+   receipt_date_time = datetime.now()-timedelta(days=time_filter)
+   date_receipts = db.read_filter(Receipt, f"time_stamp >= {receipt_date_time}")
+
+
+
