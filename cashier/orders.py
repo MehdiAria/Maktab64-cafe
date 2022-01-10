1bd66d7473ffae464392dde14b9e75cd916c19bd
MehdiAria <mahdishahidi010@gmail.com>
2022-01-10 16:13:10 +0330

    Feat: datatime re-work

--- cashier/orders.py
+++ cashier/orders.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from core.db_manager import DBManager
 from models.model import Order
+from datetime import datetime
 
 db = DBManager()
 
@@ -8,5 +9,10 @@ db = DBManager()
 def orders():
     order_list = db.all_query(Order,
                               f"""SELECT * FROM orders WHERE True;""")
+    order_list = list(map(lambda x: datetime.strptime('T'.join(x.time_stamp.__str__().split()), '%Y-%m-%dT%H:%M:%S.%f'), order_list))
     data = {'order_list': order_list}
-    return render_template('cashier/orders.html', data=data)
+    return order_list2
+
+
+print(orders())
+# return render_template('cashier/orders.html', data=data)
