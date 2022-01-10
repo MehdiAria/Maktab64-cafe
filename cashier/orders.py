c5685fbeafa75b97ea7a6e07f6462bc70eea2b61
MehdiAria <mahdishahidi010@gmail.com>
2022-01-10 14:51:43 +0330

    Feat: order class is_del

--- cashier/orders.py
+++ cashier/orders.py
@@ -7,6 +7,6 @@ db = DBManager()
 
 def orders():
     order_list = db.all_query(Order,
-                              f"""SELECT * FROM orders WHERE True;)""")
+                              f"""SELECT * FROM orders WHERE True;""")
     data = {'order_list': order_list}
     return render_template('cashier/orders.html', data=data)
