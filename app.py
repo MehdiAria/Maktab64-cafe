3d9271c2a73f70a6facaf33a812c3c918ef3916e
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-10 19:08:08 +0330

    reformat: receipts route

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.add_url_rule("/panel", "panel", panel)
 app.add_url_rule("/order/<string:table_id>", "order", order, methods=['GET', 'POST'])
 app.add_url_rule("/cashier/edit_items", "edit_items", edit_items.edit_items)
 app.add_url_rule("/cashier/orders", "orders", orders.orders)
-app.add_url_rule("/cashier/date_receipts/<int:time_filter>", "date_receipts", time_receipts)
+app.add_url_rule("/cashier/receipts", "date_receipts", all_receipts)
 # app.add_url_rule("cashier/served_orders", "served_orders", served_orders)
 app.add_url_rule("/cashier/tables", "tables", tables.tables)
 
--- cashier/receipts_view.py
+++ cashier/receipts_view.py
@@ -6,13 +6,14 @@ from datetime import datetime, timedelta
 db = DBManager()
 
 
-def time_receipts(time_filter=0):
-    date_time = datetime.now()-timedelta(days=time_filter)
-    data = db.all_query(Receipt, f"""SELECT * FROM receipt WHERE time_stamp >= CAST ('{date_time}' AS timestamp)""")
-    print(data)
-    print(len(data))
-    return render_template("cashier/receipts.html", data=data)
+# def time_receipts(time_filter=0):
+#     date_time = datetime.now() - timedelta(days=time_filter)
+#     data = db.all_query(Receipt, f"""SELECT * FROM receipt WHERE time_stamp >= CAST ('{date_time}' AS timestamp)""")
+#     print(data)
+#     print(len(data))
+#     return render_template("cashier/receipts.html", data=data)
 
-# def all_receipts():
-#     data = db.read_all(Receipt)
-#     return render_template("cashier/receipts.html")
+
+def all_receipts():
+    data = db.read_all(Receipt)
+    return render_template("cashier/receipts.html", data=data)
