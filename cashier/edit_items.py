861cd2329ee1c68cf28b202c1a0a9e17a1041493
m.kh <khosroshahi89@gmail.com>
2022-01-11 17:46:25 +0330

    reformat: edit_items.py

--- cashier/edit_items.py
+++ cashier/edit_items.py
@@ -1,15 +1,15 @@
-from flask import render_template,request
+from flask import render_template, request
 from core.db_manager import DBManager
 from models.model import MenuItems
 
-
 db = DBManager()
+
 
 def edit_items():
     items = db.read_all(MenuItems)
-    if request.method =='GET':
+    if request.method == 'GET':
         return render_template('cashier/edit_items.html', data=items)
-    elif request.method =='POST':
+    elif request.method == 'POST':
         item = db.read(MenuItems, int(request.form.get('_id')))
         item.category_id = request.form.get('category_id')
         item.discount = request.form.get('discount')
@@ -18,8 +18,5 @@ def edit_items():
         item.serving_time = request.form.get('serving_time')
         db.update(item)
         return "success"
-
-
-
 
     return render_template('cashier/edit_items.html')
