9a12efc1f875e383c642c17da6e6582ce2496ae2
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-13 21:44:38 +0330

    Add: find_category

--- models/menu_funcs.py
+++ models/menu_funcs.py
@@ -2,3 +2,10 @@ from models.model import Category, db, MenuItems
 
 all_categories = db.read_all(Category)
 all_menu_items = db.read_all(MenuItems)
+
+
+def find_category(c_id):
+    for category in all_categories:
+        category: Category
+        if category._id == c_id:
+            return category
