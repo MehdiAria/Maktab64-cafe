5e54c5f0e9ef16a51af9976606192c742b10d465
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-13 21:50:07 +0330

    Add: category_parent, category_parent_dict

--- models/menu_funcs.py
+++ models/menu_funcs.py
@@ -9,3 +9,22 @@ def find_category(c_id):
         category: Category
         if category._id == c_id:
             return category
+
+
+def category_parent(category):
+    category: Category
+    parent_id = category.category_id
+    if parent_id:
+        return category_parent(find_category(parent_id))
+    return category
+
+
+def category_parent_dict():
+    cat_dict = {}
+    for category in all_categories:
+        category: Category
+        cat_dict[category._id] = category_parent(category).name
+    return cat_dict
+
+
+
