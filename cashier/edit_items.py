551cbd63c0047374cd2ee428166cdf1ad2b32a31
MehdiAria <mahdishahidi010@gmail.com>
2022-01-16 01:21:09 +0330

    feat: edit items update

--- cashier/edit_items.py
+++ cashier/edit_items.py
@@ -14,11 +14,16 @@ def edit_items():
     if request.method == 'GET':
         return render_template('cashier/edit_items.html', cat=cat_list, data=item)
     elif request.method == 'POST':
-        item.category_id = request.form.get('category_id')
+        # item.category_id = request.form.get('category_id')
+        item=db.read(MenuItems, int(request.form.get('_id')))
         item.discount = request.form.get('discount')
+        item.name = request.form.get('name')
         item.price = request.form.get('price')
-        item.image_url = request.form.get('image_url')
         item.serving_time = request.form.get('serving_time')
+        cat_name = request.form.get('cat_name')
+        st = db.read_filter(Category, f"name=\'{cat_name}\'")[0]._id
+        item.category_id = st
+        print(st)
         db.update(item)
         return "success"
 
