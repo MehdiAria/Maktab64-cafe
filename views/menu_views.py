3a3c2775368d056ecd164c385e102b6a048ad6ac
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-15 18:59:03 +0330

    fix: status_id for orders

--- views/menu_views.py
+++ views/menu_views.py
@@ -64,7 +64,7 @@ def order(table_id):
             number_item = order_dict.get('number_item', None)
             check_table_id(receipt_id, table_id)
             table_order = Order(item_id=item_id, table_id=table_id,
-                                status_id=0, number_item=number_item, receipt_id=receipt_id)
+                                status_id=1, number_item=number_item, receipt_id=receipt_id)
             db.create(table_order)
             receipt = db.read_filter(Receipt, f"id = {receipt_id} AND user_token = \'{user_token}\'")[0]
             # TODO handel error in reading receipt
@@ -85,7 +85,7 @@ def order(table_id):
                 receipt = Receipt(total_price=price, final_price=0)
                 db.create(receipt)
             table_order = Order(item_id=order_dict.get('item_id'), table_id=table_id,
-                                status_id=0, number_item=order_dict.get('number_item'), receipt_id=receipt._id)
+                                status_id=1, number_item=order_dict.get('number_item'), receipt_id=receipt._id)
             resp.set_cookie("receipt_id", f"{receipt._id}", expires=datetime.now() + timedelta(days=1))
             db.create(table_order)
             new_token = set_user_token(receipt)
